2395bc7819ba515898497a2499d4c0c0ad50f6db
poti <poti@kaoz.org>
2026-05-03 11:23:11 +0200

    fix: add links and version to splash screen

--- frontend/src/apps/splash/components/BaseCard.tsx
+++ frontend/src/apps/splash/components/BaseCard.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import { Card } from "flowbite-react";
 import Logo from "@core/logo/Logo";
+import { useAppConfig } from "@core/config/useConfig";
 
 interface BaseCardProps {
   children: ReactNode;
@@ -8,6 +9,8 @@ interface BaseCardProps {
 }
 
 const BaseCard: React.FC<BaseCardProps> = ({ children, title = null }) => {
+  const { version } = useAppConfig();
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Card className="max-w-sm bg-white/15 border-color-[#00F5FF]/90 backdrop-blur-sm">
@@ -15,11 +18,21 @@ const BaseCard: React.FC<BaseCardProps> = ({ children, title = null }) => {
           <Logo className="h-7 w-7 text-[#00F5FF]" />
           Billedapparat
         </h5>
-
         <div className="my-3 text-white">
           {title && <h2 className="text-xl mb-2">{title}</h2>}
 
-          {children}
+          <div className="text-gray-300">{children}</div>
+        </div>
+
+        <div className="my-3 smaller border-t-gray-600 border-t">
+          Version:{" "}
+          <a
+            href="https://github.com/potibm/billedapparat/releases"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {version}
+          </a>
         </div>
       </Card>
     </div>
--- frontend/src/apps/splash/pages/Splash.tsx
+++ frontend/src/apps/splash/pages/Splash.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import BaseCard from "../components/BaseCard";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import ViewCarouselIcon from "@mui/icons-material/ViewCarousel";
+import { Link } from "react-router-dom";
 
 export const Splash: React.FC = () => {
   return (
-    <BaseCard title="Welcome">
-      <p>We will offer some options here in the future.</p>
+    <BaseCard>
+      <p>
+        <p className="mb-2">A screen director for demoparties</p>
+
+        <Link to="/beamer">
+          <ViewCarouselIcon fontSize="large" color="inherit" className="m-2" />
+          Beamer
+        </Link>
+
+        <br />
+
+        <Link to="/admin">
+          <AdminPanelSettingsIcon
+            fontSize="large"
+            color="inherit"
+            className="m-2"
+          />
+          Admin
+        </Link>
+      </p>
     </BaseCard>
   );
 };
