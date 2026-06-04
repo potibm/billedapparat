8a8c496b267f7db54709c12b9c306dae4ba9660e
poti <poti@kaoz.org>
2026-06-04 18:38:56 +0200

    fix: normalize language code to 'en'

--- backend/internal/app/collectors/pouet/parser_test.go
+++ backend/internal/app/collectors/pouet/parser_test.go
@@ -31,7 +31,7 @@ func TestParse_FromRealHTML(t *testing.T) {
 		Source:          collectorName,
 		ExternalID:      externalURLPrefix + "733fc74806d629c29d75eac9164432020e55d2400210eefa1b062e85902c9d52",
 		Body:            "Ha...  ha.......",
-		Language:        "en-EN",
+		Language:        "en",
 		MediaURLs:       nil,
 		OriginCreatedAt: result[1].OriginCreatedAt,
 		Author: &contracts.IngestSlideRequestAuthor{
@@ -49,7 +49,7 @@ func TestParse_FromRealHTML(t *testing.T) {
 		ExternalID: externalURLPrefix + "275596469a22cd65b23f854bef25dfb79fe1af030543be16454a4b634148dfc3",
 		Body: "Just a wonderful SID tune: https://deepsid.chordian.net/" +
 			"?file=/MUSICIANS/N/Nygaard_Richard/Thats_the_Wave_I t_Is.sid",
-		Language:        "en-EN",
+		Language:        "en",
 		MediaURLs:       nil,
 		OriginCreatedAt: result[0].OriginCreatedAt,
 		Author: &contracts.IngestSlideRequestAuthor{
