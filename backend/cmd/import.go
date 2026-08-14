3c0a871102452af5ff404e8594fbf0dff93f3712
poti <poti@kaoz.org>
2026-08-14 22:49:37 +0200

    fix: use configurable collector_url for slide import

--- backend/cmd/import.go
+++ backend/cmd/import.go
@@ -1,12 +1,8 @@
 package cmd
 
 import (
-	"github.com/potibm/billedapparat/internal/app/config"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-var importPort int
 
 func NewImportCmd() *cobra.Command {
 	cmd := &cobra.Command{
@@ -16,10 +12,6 @@ func NewImportCmd() *cobra.Command {
 			skipConfigValidationAnnotation: "true",
 		},
 	}
-
-	cmd.PersistentFlags().
-		IntVarP(&importPort, portFlagName, "p", config.DefaultPort, "Set the port number where the server listens on")
-	_ = viper.BindPFlag("app.port", cmd.Flags().Lookup(portFlagName))
 
 	return cmd
 }
